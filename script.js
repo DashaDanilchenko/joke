ce3f7193f18b60614e34a062be087bdde22b43fb
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-09-13 11:35:00 +0300

    fixing crashe

--- script.js
+++ script.js
@@ -35,11 +35,15 @@ btnCaterogies.addEventListener('click', activeCaterogies)
 btnWord.addEventListener('click', activeWord)
 btnJoke.addEventListener('click', createJoke)
 
+let arrAllJoke = []
+
 function reset() {
     btnOff.remove()
     arrBtn.forEach(btn => btn.classList.remove('on'))
     caterogies.classList.add('collapse')
     inputInput.classList.add('collapse')
+    arrAllJoke = []
+    createCard()
 }
 
 function activeRandom() {
@@ -74,6 +78,7 @@ function activeWord() {
 }
 
 let joke = {}
+let arrJoke = []
 
 async function jokeRandom() {
     joke = await fetch('https://api.chucknorris.io/jokes/random')
@@ -93,43 +98,41 @@ async function seeWord() {
 async function jokeWord(query) {
     await fetch(`https://api.chucknorris.io/jokes/search?query=${query}`)
     .then(date => date.json())
-    .then(date => {
-        let num = Math.random()*1000
-        let index = date.total 
-        let i = Math.ceil(num%index)
-        joke = date.result[i]
-    })
+    .then(date => {arrJoke = date.result})
+    console.log(arrJoke)
 }
 
 async function searchJoke() {
     if (btnRandom.className.includes('on')) {
        await jokeRandom()
+       arrAllJoke.push(joke)
     }
     if (btnCaterogies.className.includes('on')) {
         if (!category) {
             alert('Select a category!')
         }
        await jokeCaterogies(category)
+       arrAllJoke.push(joke)
     }
     if (btnWord.className.includes('on')) {
        await seeWord()
+       arrAllJoke = arrJoke
     }
 }
 
 async function createJoke() {
     event.preventDefault()
     await searchJoke()
-    await createCard(joke)
+    await createCard()
 }
 
 const containerCards = document.querySelector('.container_cards')
 const containerCardsLike = document.querySelector('.container_cards_like')
 const containerCardsLikesave = document.querySelector('.container_cards_save')
-let arrAllJoke = []
 let arrLikeJoke = []
 let storage = JSON.parse(localStorage.getItem('arrLikeJoke')) || []
 
-async function arrAllLikeJoke() {
+function arrAllLikeJoke() {
     arrLikeJoke.forEach(jokel => {
         storage.forEach(jokeSt => {
             if(jokel !== jokeSt) {
@@ -139,32 +142,34 @@ async function arrAllLikeJoke() {
     })
 }
 
-async function createCard({created_at, id, url, value, categories}) {
-    arrAllJoke.push(joke)
-    const card = document.createElement('div')
-    containerCards.prepend(card)
-    let created = new Date(String(created_at))
-    let hours = parseInt((Date.now() - created) / (1000 * 60 * 60), 10)
-    card.innerHTML = `<div class="card">
-    <div class="icon">
-        <div class="circle"><i class="fa-sharp fa-solid fa-comment gray"></i></div>
-    </div>
-    <div class="info">    
-        <div class="hard icon_hard ${id}">
-        <i class="fa-regular fa-heart like red" id="${id}"></i>
-        <i class="fa-solid fa-heart no_like red delete" id="${id}"></i>
-        </div>
-        <div class="id">ID: <a href="${url}">${id}</a>
-            <i class="fa-solid fa-arrow-up-right-from-square blue"></i>
-        </div>
-        <div class="text">${value}</div>
-        <div class="text_info">   
-            <div class="date">Last update: <span>${hours} hours ago</span></div>                        
-            <div class="celebrity">${categories}</div>
-        </div>
-    </div> 
-</div> `
-const b = card.querySelector('.celebrity')
+async function createCard() {
+   
+    containerCards.innerHTML = `${
+        arrAllJoke.map((i) => {
+            let created = new Date(String(i.created_at))
+            let hours = parseInt((Date.now() - created) / (1000 * 60 * 60), 10)
+          return `<div class="card">
+          <div class="icon">
+              <div class="circle"><i class="fa-sharp fa-solid fa-comment gray"></i></div>
+          </div>
+          <div class="info">    
+          <div class="hard icon_hard ${i.id}">
+          <i class="fa-regular fa-heart like red" id="${i.id}"></i>
+          <i class="fa-solid fa-heart no_like red delete" id="${i.id}"></i>
+          </div>
+              <div class="id">ID: <a href="${i.url}">${i.id}</a>
+                  <i class="fa-solid fa-arrow-up-right-from-square blue"></i>
+              </div>
+              <div class="text">${i.value}</div>
+              <div class="text_info">   
+                  <div class="date">Last update: <span>${hours} hours ago</span></div>                        
+                  <div class="celebrity">${i.categories}</div>
+              </div>
+          </div> 
+      </div> `}).join('') 
+    }`
+
+const b = document.querySelector('.celebrity')
 if (b.innerHTML === '') {
     b.classList.add('collapse') 
     }
@@ -214,7 +219,7 @@ async function createCardLike() {containerCardsLike.innerHTML = `${
           <div class="circle"><i class="fa-sharp fa-solid fa-comment gray"></i></div>
       </div>
       <div class="info">    
-          <div class="hard"><i class="fa-solid fa-heart del_like red" id="${i.id}"></i></div>
+          <div class="hard ${i.id}" ><i class="fa-solid fa-heart del_like red" id="${i.id}"></i></div>
           <div class="id">ID: <a href="${i.url}">${i.id}</a>
               <i class="fa-solid fa-arrow-up-right-from-square blue"></i>
           </div>
@@ -275,6 +280,5 @@ async function clearContainer () {
     arrLikeJoke = []
     storage = []
     await  createCardLike()
-    btnDelete.classList.remove('collapse')
 }
    btnDelete.addEventListener('click', clearContainer)
