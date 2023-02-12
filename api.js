ae3f9e807b685f514ee32b729ec5659b06c2acb4
dasha <dasha.danilchenko1990@gmail.com>
2023-02-12 17:39:22 +0200

    delete element

--- api.js
+++
@@ -1,7 +0,0 @@
-import { baseUrl } from './config';
-
-export async function getJoke(url = `${baseUrl}/random`) {
-    return await fetch(url)
-        .then(date => date.json())    
-        .then(joke => joke.result ? joke.result : [joke])
-}
