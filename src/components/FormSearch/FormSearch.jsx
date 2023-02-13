085b5837b8dca3ae86e18b439531844902937b07
dasha <dasha.danilchenko1990@gmail.com>
2023-02-13 12:44:40 +0200

    add url

--- src/components/FormSearch/FormSearch.jsx
+++ src/components/FormSearch/FormSearch.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import FromCategories from "./FromCategories"
 import Random from "./Random"
 import Search from "./Search"
@@ -5,6 +6,11 @@ import Search from "./Search"
 
 
 const FormSearch = () => {
+
+  const baseUrl = 'https://api.chucknorris.io/jokes/'
+
+  const [arrJoke, setArrJoke] = useState ([])
+  const [joke, setJoke] = useState ({})
 
   function addNone() {
     document.querySelector("#btn_container").classList.add("none")
@@ -22,15 +28,34 @@ const FormSearch = () => {
     addNone()
     document.getElementById(`${id}`).checked = true;
     if (id === "random") {
-      console.log("random")
+      getJoke(`${baseUrl}/random`)
     }
     if (id === "from_categories") {
       document.querySelector("#btn_container").classList.remove("none")
+      getJoke(`${baseUrl}/random?category=sport`)
     } 
     if (id === "search") {
       document.querySelector("#input_container").classList.remove("none")
+      getArrJoke(`${baseUrl}/search?query=stop`)
     } 
   }
+
+  function getJoke(url) {
+    fetch(url)
+        .then(date => date.json())    
+        .then(joke => setJoke(joke))
+  }
+
+  console.log(joke)
+
+
+  function getArrJoke(url) {
+    fetch(url)
+        .then(date => date.json())    
+        .then(joke => setArrJoke(joke))
+  }
+
+  console.log(arrJoke)
   
   
   return (
