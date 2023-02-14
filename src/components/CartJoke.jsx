9d2aceb69a4d63598224f6c0813569903c50d474
dasha <dasha.danilchenko1990@gmail.com>
2023-02-14 14:38:39 +0200

    add styles for card

--- src/components/CartJoke.jsx
+++ src/components/CartJoke.jsx
@@ -1,36 +1,39 @@
+import { BiMessageAltDetail, BiHeart, BiChevronRight } from "react-icons/bi"
+import styles from '../styles/CartJoke.module.css'
+
 const CartJoke = (item) => {
 
-    // const { created_at, id, url, value, categories } = item
-    // let created = new Date(String(created_at))
-    // let hours = parseInt((Date.now() - created) / (1000 * 60 * 60), 10)
-    // const template = document.createElement('div')
+    const { created_at, id, url, value, categories } = item
+    let created = new Date(String(created_at))
+    let hours = parseInt((Date.now() - created) / (1000 * 60 * 60), 10)
 
   return (
    
-    //    <div class="card">
-    //       <div class="icon">
-    //           <div class="circle"><i class="fa-sharp fa-solid fa-comment gray"></i></div>
-    //       </div>
-    //       <div class="info">    
-    //       <div class="hard icon_hard ${id}">
-    //       <button class="like-button">
-    //         ${
-    //         isFavorite(id)
-    //           ? '<i class="fa-solid fa-heart no_like red" id="${id}"></i>'
-    //           : '<i class="fa-regular fa-heart like red" id="${id}"></i>' 
-    //          }
-    //       </button>
-    //       </div>
-    //           <div class="id">ID: <a href="${url}">${id}</a>
-    //               <i class="fa-solid fa-arrow-up-right-from-square blue"></i>
-    //           </div>
-              <div>{item.value}</div>
-    //           <div class="text_info">   
-    //               <div class="date">Last update: <span>${hours} hours ago</span></div>                        
-    //               <div class="celebrity">${categories}</div>
-    //           </div>
-    //       </div> 
-    //   </div> 
+       <div className={styles.card}>
+          <div className={styles.icon}>
+              <div className={styles.circle}><BiMessageAltDetail/></div>
+          </div>
+          <div className={styles.info}>    
+          <div className={styles.hard}>
+          <button className={styles.likeButton}>
+            <BiHeart />
+            {/* ${
+            isFavorite(id)
+              ? '<i class="fa-solid fa-heart no_like red" id="${id}"></i>'
+              : '<i class="fa-regular fa-heart like red" id="${id}"></i>' 
+             } */}
+          </button>
+          </div>
+              <div className={styles.id}>ID: <a href={url}>{id}</a>
+                  <BiChevronRight/>
+              </div>
+              <div className={styles.text}>{value}</div>
+                  <div className={styles.text_info}>   
+                       <div className={styles.date}>Last update: <span>{hours} hours ago</span></div>                        
+                       <div className={styles.celebrity}>{categories}</div>
+                   </div>
+               </div> 
+           </div> 
     
   )
 }
